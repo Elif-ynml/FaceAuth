e662aaeebc16768b1c0b0b74324225171fd93924
Elif-ynml <elifyenimol@hotmail.com>
2024-03-25 21:43:10 +0300

    veritabanı dı şında herşey hazıtr gibi

--- app.py
+++ app.py
@@ -1,21 +1,69 @@
-from flask import Flask, render_template, request, app
+from flask import Flask, render_template, Response,request
 
-app=Flask(__name__)
+import cv2
 
-#anasayfa
+app = Flask(__name__)
+
+# Anasayfa
 @app.route('/anasayfa')
 def anasayfa():
     return render_template("index.html")
 
-#giriş yap
+# Giriş yap
 @app.route('/giris')
 def giris():
     return render_template("login.html")
 
-#kayıt ol
+# Kayıt ol
 @app.route('/kayit')
 def kayit():
     return render_template("register.html")
+
+# Yüz tanıma
+camera = cv2.VideoCapture(0)
+
+def generate_frames():
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            # Yüz tespiti için OpenCV kullanarak kameradan alınan görüntüyü işle
+            face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+            # Tespit edilen yüzlerin etrafına dikdörtgen çiz
+            for (x, y, w, h) in faces:
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+            # Görüntüyü JPEG formatında encode et
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+
+            # Flask'a göndermek için frame'i yield et
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    # Flask'a görüntü verilerini döndürmek için Response objesi kullan
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/form', methods=['GET', 'POST'])
+def form_verileri():
+    if request.method == 'POST':
+        # POST isteği ile gönderilen form verilerini alın
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        face_data = request.form['faceData']  # Eğer yüz verisi de gönderiliyorsa, bu şekilde alınabilir
+        
+        # Bu verileri bir HTML sayfasında göstermek için render_template fonksiyonunu kullanın
+        return render_template('form.html', username=username, password=password, email=email, face_data=face_data)
+    elif request.method == 'GET':
+        # GET isteği ile erişildiğinde, boş bir form gösterilebilir veya başka bir işlem yapılabilir
+        return render_template('form.html')  # Örnek olarak form.html'i gösteriyoruz
 
 if __name__ =="__main__":
     app.run(debug=True)
--- static/js/register-script.js
+++ static/js/register-script.js
@@ -1,74 +1,93 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const registerForm = document.getElementById("register-form");
-    const errorMessage = document.getElementById("error-message");
-    const successMessage = document.getElementById("success-message");
-    const loginButton = document.getElementById("login-btn");
-    
-    loginButton.addEventListener("click", function() {
-        window.location.href = "/giris"; // Giriş sayfasına yönlendirme
-      });
-    
-    let videoStream;
-  
-    const startCameraButton = document.getElementById("start-camera-btn");
-  
-    startCameraButton.addEventListener("click", function() {
+  const registerForm = document.getElementById("register-form");
+  const errorMessage = document.getElementById("error-message");
+  const successMessage = document.getElementById("success-message");
+  const loginButton = document.getElementById("login-btn");
+
+  loginButton.addEventListener("click", function() {
+      window.location.href = "/giris"; // Giriş sayfasına yönlendirme
+  });
+
+  let videoStream;
+  let canvas = document.getElementById("canvas");
+  let context = canvas.getContext("2d");
+
+  const startCameraButton = document.getElementById("start-camera-btn");
+
+  startCameraButton.addEventListener("click", function() {
       startCamera();
-    });
-  
-    function startCamera() {
+  });
+
+  function startCamera() {
       navigator.mediaDevices.getUserMedia({ video: true })
-        .then(function(stream) {
-          videoStream = stream;
-          const video = document.createElement("video");
-          video.setAttribute("id", "video");
-          video.setAttribute("width", "320");
-          video.setAttribute("height", "240");
-          video.srcObject = stream;
-          video.autoplay = true;
-          document.getElementById("video-container").innerHTML = "";
-          document.getElementById("video-container").appendChild(video);
-        })
-        .catch(function(err) {
-          console.error("Kamera erişimi reddedildi: ", err);
-        });
-    }
-  
-    registerForm.addEventListener("submit", function(event) {
+          .then(function(stream) {
+              videoStream = stream;
+              const video = document.createElement("video");
+              video.setAttribute("id", "video");
+              video.setAttribute("width", "320");
+              video.setAttribute("height", "240");
+              video.srcObject = stream;
+              video.autoplay = true;
+              document.getElementById("video-container").innerHTML = "";
+              document.getElementById("video-container").appendChild(video);
+
+              detectFace();
+          })
+          .catch(function(err) {
+              console.error("Kamera erişimi reddedildi: ", err);
+          });
+  }
+
+  function detectFace() {
+      const video = document.getElementById("video");
+      const faceDataInput = document.getElementById("faceData");
+      const inputWidth = video.videoWidth;
+      const inputHeight = video.videoHeight;
+
+      canvas.width = inputWidth;
+      canvas.height = inputHeight;
+
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      context.drawImage(video, 0, 0, inputWidth, inputHeight);
+
+      const faceData = canvas.toDataURL("image/jpeg");
+
+      // Yüz verisini form alanına ekleyin
+      faceDataInput.value = faceData;
+
+      requestAnimationFrame(detectFace);
+  }
+
+  registerForm.addEventListener("submit", function(event) {
       event.preventDefault();
-  
+
       const username = document.getElementById("username").value;
       const password = document.getElementById("password").value;
       const email = document.getElementById("email").value;
-      const faceData = "Yüz verisi"; // Yüz verisi olarak kabul ediyoruz
-  
-      
-       fetch('/register', {
-       method: 'POST',
-       body: JSON.stringify({ username, password, email, faceData }),
-       headers: {
-          'Content-Type': 'application/json'
-        }
-       })
-       .then(response => response.json())
-       .then(data => {
-         console.log('Kayıt başarılı:', data);
-         
-         window.location.href = "/giris"; // Giriş sayfasına yönlendir
-       })
+      const faceData = document.getElementById("faceData").value;
+
+      // Kayıt formunu gönder
+      fetch('/kayit', {
+          method: 'POST',
+          body: new FormData(registerForm), // Form verilerini direkt olarak gönder
+      })
+      .then(response => response.json())
+      .then(data => {
+          console.log('Kayıt başarılı:', data);
+          window.location.href = "/giris"; // Giriş sayfasına yönlendir
+      })
       .catch(error => {
-         console.error('Kayıt sırasında hata:', error);
-       // Kayıt sırasında hata oluştuğunda kullanıcıya bilgi verilebilir
-       });
-  
+          console.error('Kayıt sırasında hata:', error);
+          // Kayıt sırasında hata oluştuğunda kullanıcıya bilgi verilebilir
+      });
+
       console.log("Kullanıcı adı:", username);
       console.log("Şifre:", password);
       console.log("E-posta:", email);
       console.log("Yüz verisi:", faceData);
-  
+
       // Örneğin, başarılı kayıt olduğunu varsayalım
-      errorMessage.texxtContent ="Kayıt başarıyla tamamlanamadı";
+      errorMessage.textContent = "Kayıt başarıyla tamamlanamadı";
       successMessage.textContent = "Kayıt başarıyla tamamlandı!";
-       // Giriş sayfasına yönlendir
-    });
   });
+});
